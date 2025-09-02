09a63746096193f69c738564c237505c568c572e
Tiji <sjunesuh@gmail.com>
2025-09-02 17:46:21 +0800

    Fix text on setup screen being transparent

--- src/client/java/com/tiji/media/SetupScreen.java
+++ src/client/java/com/tiji/media/SetupScreen.java
@@ -18,17 +18,17 @@ public class SetupScreen extends LightweightGuiDescription {
 
         WLabel welcomeLabel = new WLabel(Text.translatable("ui.media.welcome"));
         welcomeLabel.setHorizontalAlignment(HorizontalAlignment.CENTER);
-        welcomeLabel.setColor(0x000000).setDarkmodeColor(0xFFFFFF);
+        welcomeLabel.setColor(0xFF000000).setDarkmodeColor(0xFFFFFFFF);
         root.add(welcomeLabel, 100, 20, 100, 20);
 
         WLabel setupLabel1 = new WLabel(Text.translatable("ui.media.welcome.subtext1"));
         setupLabel1.setHorizontalAlignment(HorizontalAlignment.CENTER);
-        setupLabel1.setColor(0x000000).setDarkmodeColor(0xFFFFFF);
+        setupLabel1.setColor(0xFF000000).setDarkmodeColor(0xFFFFFFFF);
         root.add(setupLabel1, 100, 50, 100, 20);
 
         WLabel setupLabel2 = new WLabel(Text.translatable("ui.media.welcome.subtext2"));
         setupLabel2.setHorizontalAlignment(HorizontalAlignment.CENTER);
-        setupLabel2.setColor(0x000000).setDarkmodeColor(0xFFFFFF);
+        setupLabel2.setColor(0xFF000000).setDarkmodeColor(0xFFFFFFFF);
         root.add(setupLabel2, 100, 65, 100, 20);
 
         WButton linkLabel = new WButton(Text.literal("http://127.0.0.1:25566").formatted(Formatting.UNDERLINE));
